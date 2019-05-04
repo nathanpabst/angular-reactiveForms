fe1175db0e23b0734ae556e650fdd461c7d8184b
Nathan Pabst <Nathan.Pabst@ey.com>
2019-05-04 15:12:30 -0500

    setting and patching initial field values

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,16 +21,25 @@ export class AppComponent implements OnInit {
       gender: new FormControl('male'),
       hobbies: new FormArray([])
     });
-    // this.signupForm.valueChanges.subscribe(
-    //   (value) => console.log(value)
-    // );
-    this.signupForm.statusChanges.subscribe(
-      (status) => console.log(status)
-    );
+    this.signupForm.setValue({
+      userData: {
+        username: 'natz',
+        email: 'natz.pagst@blah.com'
+      },
+      gender: 'male',
+      hobbies: []
+    });
+
+    this.signupForm.patchValue({
+      userData: {
+        email: 'bob@lawblogs.com'
+      }
+    });
   }
 
   onSubmit() {
     console.log(this.signupForm);
+    this.signupForm.reset();
   }
 
   onAddHobby() {
